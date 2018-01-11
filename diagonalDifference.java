22454b5a78b46e50caa5acc6b80d47db069c3c24
Dipesh Jain <jain.dipesh34@gmail.com>
2018-01-11 23:03:26 +0530

    Add files via upload

---
+++ diagonalDifference.java
@@ -0,0 +1,46 @@
+/* Given a square matrix of size , calculate the absolute difference between the sums of its diagonals.
+
+Input Format
+
+The first line contains a single integer, . The next  lines denote the matrix's rows, with each line containing space-separated integers describing the columns.
+
+Constraints
+
+Output Format
+
+Print the absolute difference between the two sums of the matrix's diagonals as a single integer. */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int diagonalDifference(int[][] a, int n) {
+        int i,j,one=0,two=0;
+        
+        for(i=0;i<n;i++)
+            one = one + a[i][i];
+        
+        for(i=0;i<n;i++)
+            two = two + a[i][n-i-1];
+        
+        return Math.abs(one-two);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] a = new int[n][n];
+        for(int a_i = 0; a_i < n; a_i++){
+            for(int a_j = 0; a_j < n; a_j++){
+                a[a_i][a_j] = in.nextInt();
+            }
+        }
+        int result = diagonalDifference(a,n);
+        System.out.println(result);
+        in.close();
+    }
+}
