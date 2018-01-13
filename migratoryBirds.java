e802e2c6f3564ddf38f2ad73333667ce74dafb03
Dipesh Jain <jain.dipesh34@gmail.com>
2018-01-13 16:30:40 +0530

    Update migratoryBirds.java

--- migratoryBirds.java
+++ migratoryBirds.java
@@ -1,7 +1,8 @@
-/* A flock of n birds is flying across the continent. Each bird has a type, and the different types are designated by the ID numbers 1, 2, 3, 4, and 5.
+/* A flock of n birds is flying across the continent. Each bird has a type, and the different types are designated by the
+ID numbers 1, 2, 3, 4, and 5.
 
-Given an array of n integers where each integer describes the type of a bird in the flock, find and print the type number of the most common bird.
-If two or more types of birds are equally common, choose the type with the smallest ID number. */
+Given an array of n integers where each integer describes the type of a bird in the flock, find and print the type number 
+of the most common bird. If two or more types of birds are equally common, choose the type with the smallest ID number. */
 
 import java.io.*;
 import java.util.*;
