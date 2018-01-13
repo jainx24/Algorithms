2d4bc9ee45fa36a1baf352abf389c68577d57e25
Dipesh Jain <jain.dipesh34@gmail.com>
2018-01-13 16:39:30 +0530

    Update sumOfLong.java

--- WarmUp/sumOfLong.java
+++ WarmUp/sumOfLong.java
@@ -1,11 +1,10 @@
-/* You are given an array of integers of size . You need to print the sum of the elements in the array, keeping in mind that some of those integers may be quite large.
+/* You are given an array of integers of size . You need to print the sum of the elements in the array, keeping in mind that some 
+of those integers may be quite large.
 
 Input Format
-
-The first line of the input consists of an integer . The next line contains  space-separated integers contained in the array.
+The first line of the input consists of an integer n. The next line contains n space-separated integers contained in the array.
 
 Output Format
-
 Print a single value equal to the sum of the elements in the array. */
 
 import java.io.*;
