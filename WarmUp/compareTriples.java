2555d8dde1a06d8ad9d515a500c4dbe1b5641d2e
Dipesh Jain <jain.dipesh34@gmail.com>
2018-01-13 16:36:54 +0530

    Update compareTriples.java

--- WarmUp/compareTriples.java
+++ WarmUp/compareTriples.java
@@ -1,20 +1,22 @@
-/* Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a scale from  to  for three categories: problem clarity, originality, and difficulty.
+/* Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges, awarding points on a 
+scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
 
-We define the rating for Alice's challenge to be the triplet , and the rating for Bob's challenge to be the triplet .
+-We define the rating for Alice's challenge to be the triplet A={a0,a1,...,an-1}, and the rating for Bob's challenge to 
+be the triplet B={b0,b1....,bm-1}.
 
-Your task is to find their comparison points by comparing  with ,  with , and  with .
+Your task is to find their comparison points by comparing a0 with b0,  a1 with b1, and a2 with b2.
 
-If ai>bi, then Alice is awarded  point.
-If ai<bi, then Bob is awarded  point.
+If ai>bi, then Alice is awarded 1 point.
+If ai<bi, then Bob is awarded 1 point.
 If ai=bi, then neither person receives a point.
 Comparison points is the total points a person earned.
 
-Given  and , can you compare the two challenges and print their respective comparison points?
+Given n and m, can you compare the two challenges and print their respective comparison points?
 
 Input Format
 
-The first line contains  space-separated integers, , , and , describing the respective values in triplet . 
-The second line contains  space-separated integers, , , and , describing the respective values in triplet .
+The first line contains n space-separated integers a0, a1... and an-1, describing the respective values in triplet A. 
+The second line contains m space-separated integers b0, b1... and bm-1, describing the respective values in triplet B.
 
 Constraints
 
