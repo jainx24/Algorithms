161d98db902d88aae76c4c616c31e3bc19c099b8
Dipesh Jain <jain.dipesh34@gmail.com>
2018-01-13 16:31:23 +0530

    Update Staircase.java

--- WarmUp/Staircase.java
+++ WarmUp/Staircase.java
@@ -3,9 +3,8 @@
   ##
  ###
 ####
-Observe that its base and height are both equal to , and the image is drawn using # symbols and spaces. The last line is not preceded by any spaces.
-
-Write a program that prints a staircase of size .
+Observe that its base and height are both equal to , and the image is drawn using # symbols and spaces. 
+The last line is not preceded by any spaces. Write a program that prints a staircase of size n.
 
 Input Format
 
