ba1f05bc3f47e7a24e5d4a1aa2dcbdd19f1128b0
Dipesh Jain <jain.dipesh34@gmail.com>
2018-01-13 16:39:03 +0530

    Update plusMinus.java

--- WarmUp/plusMinus.java
+++ WarmUp/plusMinus.java
@@ -1,16 +1,15 @@
-/* Given an array of integers, calculate which fraction of its elements are positive, which fraction of its elements are negative, and which fraction of its elements are zeroes, respectively. Print the decimal value of each fraction on a new line.
+/* Given an array of integers, calculate which fraction of its elements are positive, which fraction of its elements are negative, 
+and which fraction of its elements are zeroes, respectively. Print the decimal value of each fraction on a new line.
 
-Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to 10^-4 are acceptable.
+Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with 
+absolute error of up to 10^-4 are acceptable.
 
 Input Format
-
 The first line contains an integer, N, denoting the size of the array. 
 The second line contains N space-separated integers describing an array of numbers .
 
 Output Format
-
 You must print the following  lines:
-
 A decimal representing of the fraction of positive numbers in the array compared to its size.
 A decimal representing of the fraction of negative numbers in the array compared to its size.
 A decimal representing of the fraction of zeroes in the array compared to its size. */
