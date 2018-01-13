c473578b5482cdec4b72a71d72fe468c3bbdd0c9
Dipesh Jain <jain.dipesh34@gmail.com>
2018-01-13 16:30:15 +0530

    Update kangaroo.java

--- kangaroo.java
+++ kangaroo.java
@@ -1,7 +1,8 @@
 /* There are two kangaroos on a number line ready to jump in the positive direction (i.e, toward positive infinity). 
 The first kangaroo starts at location x1 and moves at a rate of v1 meters per jump. 
 The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump. 
-Given the starting locations and movement rates for each kangaroo, can you determine if they'll ever land at the same location at the same time?
+Given the starting locations and movement rates for each kangaroo, can you determine if they'll ever land at the 
+same location at the same time?
 
 Input Format
 
