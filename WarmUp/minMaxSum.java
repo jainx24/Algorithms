30a0c1540ce861260de199ac28379684216c05a1
Dipesh Jain <jain.dipesh34@gmail.com>
2018-01-13 16:38:36 +0530

    Update minMaxSum.java

--- WarmUp/minMaxSum.java
+++ WarmUp/minMaxSum.java
@@ -2,11 +2,9 @@
 Then print the respective minimum and maximum values as a single line of two space-separated long integers.
 
 Input Format
-
 A single line of five space-separated integers.
 
 Output Format
-
 Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. 
 (The output can be greater than 32 bit integer.) */
 
